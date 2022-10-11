63f9f1ad9210a594ad7afa6dd5af4c529b71e4e1
Lucas Ferreira <lucasfpnt@gmail.com>
2022-10-10 23:21:31 -0300

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true
 }
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development'
-  },
-  ...nextConfig
-})
 
-export default nextConfig
+module.exports = nextConfig
--- src/components/UI/Button/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ButtonContainer } from './style'
-
-const Button = () => {
-  return <ButtonContainer>Clicar</ButtonContainer>
-}
-
-export default Button
